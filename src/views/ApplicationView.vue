a0a4eace8382272cce9f6579d3b9286def09fdeb
Oliver <oliver@noles.nz>
2022-09-23 09:58:00 +1200

    Added mobile menu icon

--- src/views/ApplicationView.vue
+++ src/views/ApplicationView.vue
@@ -26,9 +26,25 @@
         <div class="relative z-10 flex items-center">
           <!-- Mobile menu button -->
           <DisclosureButton
-            class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            class="inline-flex items-center justify-center rounded-md p-2 text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
           >
-            <span>Open menu</span>
+            <svg
+              x-state:on="Menu open"
+              x-state:off="Menu closed"
+              class="block h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="white"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              ></path>
+            </svg>
           </DisclosureButton>
         </div>
       </div>
