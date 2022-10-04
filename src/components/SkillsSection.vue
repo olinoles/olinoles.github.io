0882c436075ed3373c8121e89c8e863bae4c4da0
Oliver <oliver@noles.nz>
2022-10-04 22:55:37 +1300

    Add floating animation to skills and change mobile layout

--- src/components/SkillsSection.vue
+++ src/components/SkillsSection.vue
@@ -2,7 +2,7 @@
   <div
     class="mx-8 my-20 flex flex-col items-center rounded-md p-5 dark:bg-transparent md:flex-row lg:mx-3"
   >
-    <div class="my-3 mb-10" id="skills-description">
+    <div class="my-3 mb-10 max-w-md" id="skills-description">
       <h1>My Tools & Tech</h1>
       <p>I use a range of tools and technology to achieve success.</p>
       <h3 class="skills-subheading">Development</h3>
@@ -29,22 +29,17 @@
         <HighlightSpan text="Github" :highlightFn="highlightSkill" />.
       </p>
     </div>
-    <div
-      class="skill-container mx-auto flex max-w-xl flex-row flex-wrap justify-center md:max-w-lg lg:max-w-2xl lg:px-12"
-    >
+    <div class="skill-container">
       <div
-        class="skill-icon my-3 flex flex-col"
+        class="skill-icon"
         :class="highlightedSkill === skill.name && 'highlighted-skill'"
         v-for="(skill, index) in skills"
         :key="`skill-${index}`"
       >
-        <img
-          class="mx-6 my-2 h-10 w-10 dark:brightness-90"
-          :src="darkMode ? skill.darkImage : skill.image"
-        />
-        <div
-          class="skill-text text-center text-xs font-medium text-gray-500 dark:text-gray-300 sm:opacity-0"
-        >
+        <div class="skill-image">
+          <img :src="darkMode ? skill.darkImage : skill.image" />
+        </div>
+        <div class="skill-text">
           {{ skill.name }}
         </div>
       </div>
@@ -73,12 +68,28 @@ function highlightSkill(skill: string) {
   background: radial-gradient(#152447, #0f172a, #0f172a);
   background-repeat: no-repeat;
 }
-.skill-icon img {
+.skill-container {
+  @apply mx-auto flex max-w-xl flex-row flex-wrap justify-center md:max-w-lg lg:max-w-2xl lg:px-12;
+}
+.skill-icon {
+  @apply my-3 flex flex-col items-center;
+}
+.skill-icon .skill-image {
   @apply transform duration-300 ease-in-out;
 }
-.skill-icon:hover img,
-.highlighted-skill img {
-  @apply -translate-y-3 scale-125;
+.skill-text {
+  @apply w-20 text-center text-xs font-medium text-gray-500 dark:text-gray-300 sm:opacity-0;
+}
+.skill-image {
+  @apply mx-6 my-2 flex h-10 w-10 dark:brightness-90;
+}
+.skill-icon:hover .skill-image img,
+.highlighted-skill .skill-image img {
+  @apply animate-float;
+}
+.skill-icon:hover .skill-image,
+.highlighted-skill .skill-image {
+  @apply -translate-y-3 scale-110;
 }
 .skill-icon:hover .skill-text,
 .highlighted-skill .skill-text {
@@ -102,6 +113,6 @@ function highlightSkill(skill: string) {
   }
 }
 .animate-float {
-  animation: float 1s infinite;
+  animation: float 2s infinite;
 }
 </style>
